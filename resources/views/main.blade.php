bd72c7b664ee1d4d6eeff697f980eb72a8b15fd1
Fernando Merino <jesusferm@outlook.com>
2020-06-14 21:09:48 -0500

    v0.1.2 updated view bootstrap

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-
+		<link rel="shortcut icon" type="image/png" href="{{ asset('images/favicon.png') }}"/>
 		<title>Laravel</title>
 
 		<!-- Fonts -->
@@ -78,53 +78,8 @@
 		</style>
 	</head>
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-			<div class="container-xl">
-			<a class="navbar-brand" href="#">
-				{{ config('app.name', 'Laravel') }}
-			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<div class="collapse navbar-collapse" id="navbarsExample07XL">
-				<ul class="navbar-nav mr-auto">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Link</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="dropdown07XL" data-toggle="dropdown" aria-expanded="false">Dropdown</a>
-						<div class="dropdown-menu" aria-labelledby="dropdown07XL">
-							<a class="dropdown-item" href="#">Action</a>
-							<a class="dropdown-item" href="#">Another action</a>
-							<a class="dropdown-item" href="#">Something else here</a>
-						</div>
-					</li>
-				</ul>
-			</div>
-		</div>
-		</nav>
-		<div class="flex-center position-ref full-height">
-			@if (Route::has('login'))
-				<div class="top-right links">
-					@auth
-						<a href="{{ url('/home') }}">Home</a>
-					@else
-						<a href="{{ route('login') }}">Login</a>
-
-						@if (Route::has('register'))
-							<a href="{{ route('register') }}">Register</a>
-						@endif
-					@endauth
-				</div>
-			@endif
-
+		@include('navbar')
+		<div class="flex-center position-ref full-height mt-5">
 			<div class="content">
 				<div class="title m-b-md">
 					Laravel
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,53 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+	<div class="container-xl">
+		<a class="navbar-brand" href="{{ url('/') }}">
+			{{ config('app.name', 'Laravel') }}
+		</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample07XL" aria-controls="navbarsExample07XL" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<div class="collapse navbar-collapse" id="navbarsExample07XL">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#">About</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Services</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Contact</a>
+				</li>
+				<!-- Authentication Links -->
+				@guest
+					<li class="nav-item">
+						{!!(request()->is('login')) ? '' : '<a class="btn btn-outline-info btn-sm mt-1" href="'.route('login').'">Iniciar sesión</a>'!!}
+					</li>
+					@if (Route::has('register'))
+						<li class="nav-item">
+							{!!(request()->is('register')) ? '' : '<a class="btn btn-outline-success btn-sm mt-1 ml-1" href="'.route('register').'">Registrarse</a>'!!}
+						</li>
+					@endif
+				@else
+					<li class="nav-item dropdown">
+						<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+							{{ Auth::user()->name }} <span class="caret"></span>
+						</a>
+
+						<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+							<a class="dropdown-item" href="{{ route('logout') }}"
+							   onclick="event.preventDefault();
+											 document.getElementById('logout-form').submit();">
+								{{ __('Logout') }}
+							</a>
+
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+								@csrf
+							</form>
+						</div>
+					</li>
+				@endguest
+			</ul>
+		</div>
+	</div>
+</nav>
